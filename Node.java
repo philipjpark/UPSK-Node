25be391b3da81d524b296664bb07a398a397c90b
philipjpark <66290921+philipjpark@users.noreply.github.com>
2022-01-01 23:04:29 -0500

    Creating a public class Node

---
+++ Node.java
@@ -0,0 +1,57 @@
+public class Node<E> {
+    E element;
+    Node<E> next;
+     
+    public Node(E e){
+        element = e;
+        next = null;
+    }
+    public static void main (String[] args)  {
+
+        Node<Integer> head = null, tail = null,startHead = null, tail2 = null;
+        Node<Integer> newHead = null, newTail = null, oldTail = null;
+        head = new Node<>(1);
+        tail = head;
+        tail.next = new Node<>(2);
+        tail = tail.next;
+        newHead = tail;
+        tail.next = new Node<>(3); 
+        tail = tail.next;
+        newTail = tail;
+        tail.next = new Node<>(4); 
+        tail = tail.next;
+        oldTail = tail;
+       tail = tail.next;
+        
+
+        System.out.print("Original set of nodes: ");
+        Node<Integer> walkThrough = head;
+        while(walkThrough != null) {
+            System.out.print(walkThrough.element + " ");
+        walkThrough = walkThrough.next;
+
+        }
+        
+        System.out.print("\n" + "Swapping every two adjacent nodes: ");
+        startHead = newHead;
+        tail2 = startHead; 
+        tail2.next = head;
+        tail2 = tail2.next;
+        tail2.next = oldTail;
+        tail2 = tail2.next;
+        // tail2.next = newTail; //This original code is not running. I need to refactor it. 
+        tail2.next = new Node<>(3); 
+        tail2 = tail2.next;
+       
+        Node<Integer> walkAndSwap = startHead;
+        while(walkAndSwap != null) {
+            System.out.print(walkAndSwap.element + " ");
+        walkAndSwap = walkAndSwap.next;
+        }
+        
+            }
+            
+        }
+
+    
+
